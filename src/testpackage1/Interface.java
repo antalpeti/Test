b48983d87dda5a5c214e2986604dea637a40fc9c
Peti <antalpeti@users.noreply.github.com>
2015-02-07 23:31:59 +0100

    Added interface field and method overview example.

---
+++ src/testpackage1/Interface.java
@@ -0,0 +1,7 @@
+package testpackage1;
+
+public interface Interface { // implicitly abstract
+  int someValue = 10; // implicitly public static final
+
+  int someMethod(); // implicitly public abstract
+}
---
+++ src/testpackage2/InterfaceTest.java
@@ -0,0 +1,10 @@
+package testpackage2;
+
+import testpackage1.Interface;
+
+public class InterfaceTest {
+  public static void main(String[] args) {
+    int someValue = Interface.someValue;
+    System.out.println(someValue);
+  }
+}
