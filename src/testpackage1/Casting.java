d1f28b530ff92dae4fe8941e2a373fb24137ca27
Peti <antalpeti@users.noreply.github.com>
2015-02-10 09:07:59 +0100

    Example for downcasting.

---
+++ src/testpackage1/Casting.java
@@ -0,0 +1,16 @@
+package testpackage1;
+
+public class Casting {
+  public static void main(String[] args) {
+    double d = 100.2d;
+    int i = (int) d;
+    int s = (int) d;
+    byte b = (byte) d;
+    char c = (char) d;
+    System.out.println(d);
+    System.out.println(i);
+    System.out.println(s);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
