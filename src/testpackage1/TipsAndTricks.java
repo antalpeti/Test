425493dcf3c7094c954c059b6379a9329721565a
Peti <antalpeti@users.noreply.github.com>
2015-02-22 14:47:28 +0100

    Tried out: getter and setter generation, delegate method generation, hashcode and equals generation

---
+++ src/testpackage1/TipsAndTricks.java
@@ -0,0 +1,78 @@
+package testpackage1;
+
+public class TipsAndTricks {
+  private int var;
+
+  private String name;
+
+  private static boolean isOk;
+
+  /**
+   * This is a delegated method.
+   *
+   * @param index
+   * @return
+   */
+  public char charAt(int index) {
+    return name.charAt(index);
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (name == null ? 0 : name.hashCode());
+    result = prime * result + var;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof TipsAndTricks)) {
+      return false;
+    }
+    TipsAndTricks other = (TipsAndTricks) obj;
+    if (name == null) {
+      if (other.name != null) {
+        return false;
+      }
+    } else if (!name.equals(other.name)) {
+      return false;
+    }
+    if (var != other.var) {
+      return false;
+    }
+    return true;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getVar() {
+    return var;
+  }
+
+  public void setVar(int var) {
+    this.var = var;
+  }
+
+  public static boolean isOk() {
+    return isOk;
+  }
+
+  public static void setOk(boolean isOk) {
+    TipsAndTricks.isOk = isOk;
+  }
+
+}
