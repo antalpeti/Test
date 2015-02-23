a09439e82ef97803400f2dfee7c4aa50fdcebff8
Peti <antalpeti@users.noreply.github.com>
2015-02-23 20:47:45 +0100

    Static and local variables. Global vs. local scope.

--- src/trickyexamples/Test.java
+++ src/trickyexamples/Test.java
@@ -1,9 +1,25 @@
 package trickyexamples;
 
 public class Test {
+  static int i = 2;
+
+  public static void myInt(int i) {
+    System.out.print(i);
+  }
+
   public static void main(String[] args) {
     Foo f = new Bar();
     f.add();
     System.out.println("a = " + f.a);
+
+    {
+      @SuppressWarnings("unused")
+      int i = 3;
+    }
+
+    myInt(i);
+    System.out.print(i);
+    i = 1;
+    System.out.print(i);
   }
 }
