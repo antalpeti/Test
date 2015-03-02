3f5ad7e30dec849a90ab7e8e4981c479e88add9a
Peti <antalpeti@users.noreply.github.com>
2015-03-02 18:17:44 +0100

    Added example about pass-by-value.

---
+++ src/trickyexamples/Dog.java
@@ -0,0 +1,17 @@
+package trickyexamples;
+
+public class Dog {
+  private String name;
+
+  public Dog(String name) {
+    this.setName(name);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/trickyexamples/ParameterHandling.java
@@ -0,0 +1,60 @@
+package trickyexamples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParameterHandling {
+  private static void doPrimitive(int p) {
+    p *= 2;
+  }
+
+  private static void doString(String s) {
+    s += s;
+  }
+
+  private static void doArrayList(List<String> l) {
+    l.add("something");
+  }
+
+  public static void foo(Dog d) {
+    d.getName().equals("Max"); // true
+    d = new Dog("Fifi");
+    d.getName().equals("Fifi"); // true
+  }
+
+  public static void bar(Dog d) {
+    d.getName().equals("Max"); // true
+    d.setName("Fifi");
+    d.getName().equals("Fifi"); // true
+  }
+
+  public static void main(String[] args) {
+    int p = 1;
+    String s = "str";
+    List<String> l = new ArrayList<String>();
+    System.out.println(p);
+    System.out.println(s);
+    System.out.println(l);
+    doPrimitive(p);
+    doString(s);
+    doArrayList(l);
+    System.out.println(p);
+    System.out.println(s);
+    System.out.println(l);
+
+    Dog aDog = new Dog("Max");
+    foo(aDog);
+    if (aDog.getName().equals("Max")) { // true
+      System.out.println("Java passes by value.");
+    } else if (aDog.getName().equals("Fifi")) {
+      System.out.println("Java passes by reference.");
+    }
+
+    bar(aDog);
+    if (aDog.getName().equals("Max")) {
+      System.out.println("Java passes by value.");
+    } else if (aDog.getName().equals("Fifi")) { // true
+      System.out.println("Java passes by reference.");
+    }
+  }
+}
