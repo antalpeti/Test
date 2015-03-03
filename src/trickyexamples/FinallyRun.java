35c889f11bbd957a0458c81db29e6afe2f41b05c
Peti <antalpeti@users.noreply.github.com>
2015-03-03 21:41:24 +0100

    Added example about exception occured and return from the finally block. Added method markers to the write out result.

--- src/trickyexamples/FinallyRun.java
+++ src/trickyexamples/FinallyRun.java
@@ -3,8 +3,12 @@ package trickyexamples;
 public class FinallyRun {
   public static void main(String[] args) {
     FinallyRun obj = new FinallyRun();
+    System.out.println("--returnFromTry--");
     obj.returnFromTry();
+    System.out.println("--returnFromCatch--");
     obj.returnFromCatch();
+    System.out.println("--returnFromFinally--");
+    System.out.println(obj.returnFromFinally());
   }
 
   private void returnFromTry() {
@@ -25,6 +29,21 @@ public class FinallyRun {
       return;
     } finally {
       System.out.println("This is finally block.");
+    }
+  }
+
+  @SuppressWarnings("finally")
+  private int returnFromFinally() {
+    try {
+      System.out.println("try");
+      @SuppressWarnings("unused")
+      int i = 10 / 0;
+      return 1;
+    } catch (Exception e) {
+      System.out.println("catch");
+    } finally {
+      System.out.println("finally");
+      return 2;
     }
   }
 }
