ceafe18e1fcba693191e35c02e0410228280bec5
Peti <antalpeti@users.noreply.github.com>
2015-03-16 17:01:36 +0100

    Added example about to take the minimum of NaN, + and - Infinity.

---
+++ src/trickyexamples/EqualityCheck.java
@@ -0,0 +1,16 @@
+package trickyexamples;
+
+public class EqualityCheck {
+  private static void checkNotNumbers() {
+    float result = Math.min(Float.NaN, Float.POSITIVE_INFINITY);
+    System.out.println(result);
+    result = Math.min(Float.NaN, Float.NEGATIVE_INFINITY);
+    System.out.println(result);
+    result = Math.min(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+    System.out.println(result);
+  }
+
+  public static void main(String[] args) {
+    checkNotNumbers();
+  }
+}
