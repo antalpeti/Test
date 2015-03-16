ddd0b00ae4ca475eb000284b601b892a7c0bfcb8
Peti <antalpeti@users.noreply.github.com>
2015-03-16 21:43:33 +0100

    Printed out null String.

--- src/trickyexamples/NullPrint.java
+++ src/trickyexamples/NullPrint.java
@@ -5,6 +5,8 @@ public class NullPrint {
   static String s2;
 
   public static void main(String[] args) {
+    System.out.println(s1);
+    System.out.println(s1);
     s2 = s1 + s2;
     System.out.println(s2);
   }
