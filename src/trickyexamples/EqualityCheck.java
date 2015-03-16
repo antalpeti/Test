fc2d19a4844f5dec7dd0bcf79e1f3bd09ac22853
Peti <antalpeti@users.noreply.github.com>
2015-03-16 17:02:44 +0100

    Added example about to take the maximum of NaN, + and - Infinity.

--- src/trickyexamples/EqualityCheck.java
+++ src/trickyexamples/EqualityCheck.java
@@ -8,6 +8,13 @@ public class EqualityCheck {
     System.out.println(result);
     result = Math.min(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
     System.out.println(result);
+
+    result = Math.max(Float.NaN, Float.POSITIVE_INFINITY);
+    System.out.println(result);
+    result = Math.max(Float.NaN, Float.NEGATIVE_INFINITY);
+    System.out.println(result);
+    result = Math.max(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+    System.out.println(result);
   }
 
   public static void main(String[] args) {
