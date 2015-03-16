26d52e83ba24bfcfa7c70c14c21694fe265fdf31
Peti <antalpeti@users.noreply.github.com>
2015-03-16 15:50:37 +0100

    Extended with implicitly held keywords for demonstration purpose.

--- src/keywords/InterfaceKeyword.java
+++ src/keywords/InterfaceKeyword.java
@@ -4,10 +4,10 @@ package keywords;
  * Implicitly public abstract all methods. All of its fields default public final static.
  */
 public interface InterfaceKeyword {
-  int field1 = 1;
+  public final static int field1 = 1;
   float field2 = 1f;
 
-  void method1();
+  public abstract void method1();
 
   void method2();
 
