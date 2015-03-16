3da83017ecd7c2baeebaeb4849727ef089f503ca
Peti <antalpeti@users.noreply.github.com>
2015-03-16 17:11:05 +0100

    Added example about rethrown exception and finally block usage.

--- src/trickyexamples/ExceptionHandling.java
+++ src/trickyexamples/ExceptionHandling.java
@@ -15,8 +15,29 @@ public class ExceptionHandling {
     }
   }
 
+  private void doStuff() {
+    throw new IllegalStateException();
+  }
+
+  private void rethrownException() {
+    try {
+      this.doStuff();
+    } catch (RuntimeException e) {
+      try {
+        throw new Exception();
+      } catch (Exception e1) {
+        System.out.println("Exception occured.");
+      }
+    } finally {
+      System.out.println("Finally run.");
+    }
+  }
+
   public static void main(String[] args) {
     ExceptionHandling eh = new ExceptionHandling();
+    System.out.println("--Example about Exception propagation.--");
     eh.method2();
+    System.out.println("\n--Example about rethrown exception and finally block usage--");
+    eh.rethrownException();
   }
 }
