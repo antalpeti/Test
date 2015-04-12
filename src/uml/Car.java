78f6a01faa107b433fa778a4146678cdef0f6ee8
Peti <antalpeti@users.noreply.github.com>
2015-04-12 11:20:07 +0200

    Added example about class diagram.

---
+++ src/uml/Car.java
@@ -0,0 +1,14 @@
+package uml;
+
+public class Car {
+  private String carColor;
+  private double carPrice = 0.0;
+
+  public String getCarColor(String model) {
+    return carColor;
+  }
+
+  public double getCarPrice(String model) {
+    return carPrice;
+  }
+}
--- src/uml/package-info.java
+++ src/uml/package-info.java
@@ -1,4 +1,9 @@
 /**
  * Contains some examples about usage of UML connections.
+ *
+ * <h2>Class Diagram Example</h2>
+ * <p>
+ * <img src="./class_diagram_example.gif" alt"Class Diagram Example" width="100%">
+ * </p>
  */
 package uml;
