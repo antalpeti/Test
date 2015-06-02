ed48aaf4fdfc2e0a7c47fbb57e991af49b756d85
Peti <antalpeti@users.noreply.github.com>
2015-06-02 21:06:30 +0200

    Add example about how to calculate the minimal coins and banknotes of returning in case of overpaid.

--- src/trickyexamples/CalculateReturning.java
+++ src/trickyexamples/CalculateReturning.java
@@ -16,10 +16,42 @@ public class CalculateReturning {
       for (int i = 0; i < money.length; i++) {
         pieces[i] = diff / money[i];
         diff = diff % money[i];
-        System.out.println(money[i] + " : " + pieces[i]);
+        if (pieces[i] > 0) {
+          System.out.println(money[i] + " : " + pieces[i]);
+        }
+        if (diff == 0) {
+          break;
+        }
       }
     } else {
       System.out.println("Paid more!");
+    }
+  }
+
+  public static void getVisszajaro(int fizetendo, int fizetett) {
+    System.out.println("Fizetendõ: " + fizetendo + " Fizetett: " + fizetett);
+    if (fizetendo % 5 >= 3) {
+      fizetendo += 5 - fizetendo % 5;
+    } else {
+      fizetendo -= fizetendo % 5;
+    }
+    int kulonbozet = fizetett - fizetendo;
+    if (kulonbozet > 0) {
+      System.out.println("Visszajáró: ");
+      int[] penzek = new int[] {20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5};
+      int[] darabok = new int[penzek.length];
+      for (int i = 0; i < penzek.length; i++) {
+        darabok[i] = kulonbozet / penzek[i];
+        kulonbozet = kulonbozet % penzek[i];
+        if (darabok[i] > 0) {
+          System.out.println(penzek[i] + " : " + darabok[i]);
+        }
+        if (kulonbozet == 0) {
+          break;
+        }
+      }
+    } else {
+      System.out.println("Fizess többet!");
     }
   }
 
@@ -31,5 +63,8 @@ public class CalculateReturning {
     calculateReturning(992, 4800);
     calculateReturning(12, 20);
     calculateReturning(13, 20);
+    getVisszajaro(12, 20);
+    getVisszajaro(13, 20);
+    getVisszajaro(1000, 10000);
   }
 }
